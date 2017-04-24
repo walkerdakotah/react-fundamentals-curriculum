9f969c49be465cfb122f271d663a90c16753ca7e
dakotah walker <walker.dakotah@gmail.com>
2017-04-23 23:55:56 -0700

    completes step2 views. need to integate function into zipcode component

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -3,7 +3,7 @@ var React = require('react');
 function Nav () {
   return (
     <div className='navbar'>
-      <h1>Main.js Header</h1>
+      <h1>Main Header</h1>
     </div>
   )
 }
---
+++ app/components/Zipcode.js
@@ -0,0 +1,15 @@
+var React = require('react');
+
+class Zipcode extends React.Component {
+  render() {
+    return (
+      <div className='zipcode-container' style={{flexDirection: 'column'}}>
+        <input type="text" className="form-control" placeholder="St. George, Utah" value=""/>
+        <button type="button" className="btn btn-success" style={{margin: '10px'}}>Get Weather</button>
+      </div>
+    )
+  }
+}
+
+module.exports = Zipcode;
+
